f84f05495d925ddaad50afba1161b36f0464ffd3
JBDolmus <jbautista.dormo.corea@gmail.com>
2024-09-07 19:24:35 -0600

    Agregando el .gitignore y limpiando el repositorio

--- bubblehouseapi/rol/api/serializer.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import Permission
-from rest_framework import serializers
-
-from rol.models import Rol
-
-class RolSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Rol
-        fields = ('id', 'name', 'created_at', 'state')
--- bubblehouseapi/rol/api/view.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-
-from rol.api.serializer import RolSerializer
-from rol.models import Rol
-class RolViewSet(viewsets.ModelViewSet):
-    queryset = Rol.objects.all()
-    serializer_class = RolSerializer
